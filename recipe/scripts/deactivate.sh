5d148adba840e28b058bcc70e5be88c7b7e202e7
Howard Butler <hobu.inc@gmail.com>
2022-03-10 08:49:30 -0600

    Attempt to address #120, _CONDA_SET_PROJ_LIB unsetting

--- recipe/scripts/deactivate.sh
+++ recipe/scripts/deactivate.sh
@@ -5,6 +5,6 @@ unset PROJ_LIB
 unset PROJ_NETWORK
 
 if [ -n "$_CONDA_SET_PROJ_LIB" ]; then
-    export PROJ_LIB=$_CONDA_SET_PROJ_LIB
+    export PROJ_LIB="${_CONDA_SET_PROJ_LIB:-}"
     unset _CONDA_SET_PROJ_LIB
 fi
