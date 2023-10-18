be85c00d49fb9aa0f375b3ba5e23afcb995ef0ff
David Hoese <david.hoese@ssec.wisc.edu>
2023-10-18 15:26:51 -0500

    Simplify build script

--- .scripts/run_linux_build.sh
+++ .scripts/run_linux_build.sh
@@ -82,13 +82,13 @@ echo "export PYTHONUNBUFFERED='${PYTHONUNBUFFERED}'"    >> "${CONDA_PREFIX}/etc/
 
 echo -e "\n\nDone with activate script"
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo -e "\n\nSourcing cross compile support | SCRIPT_DIR: ${SCRIPT_DIR}"
+#echo -e "\n\nSourcing cross compile support | SCRIPT_DIR: ${SCRIPT_DIR}"
 
-source ${SCRIPT_DIR}/cross_compile_support.sh
+#source ${SCRIPT_DIR}/cross_compile_support.sh
 
-echo -e "\n\nAfter cross compile support source"
+#echo -e "\n\nAfter cross compile support source"
 
 conda info
 conda config --env --show-sources
@@ -107,36 +107,14 @@ if [[ -f LICENSE.txt ]]; then
   cp LICENSE.txt "recipe/recipe-scripts-license.txt"
 fi
 
-if [[ "${BUILD_WITH_CONDA_DEBUG:-0}" == 1 ]]; then
-    if [[ "x${BUILD_OUTPUT_ID:-}" != "x" ]]; then
-        EXTRA_CB_OPTIONS="${EXTRA_CB_OPTIONS:-} --output-id ${BUILD_OUTPUT_ID}"
-    fi
-    conda debug ./recipe -m ./.ci_support/${CONFIG}.yaml \
-        ${EXTRA_CB_OPTIONS:-} \
-        --clobber-file ./.ci_support/clobber_${CONFIG}.yaml
+EXTRA_CB_OPTIONS=""
 
-    # Drop into an interactive shell
-    /bin/bash
-else
+conda mambabuild ./recipe -m ./.ci_support/${CONFIG}.yaml \
+    --suppress-variables ${EXTRA_CB_OPTIONS:-} \
+    --clobber-file ./.ci_support/clobber_${CONFIG}.yaml
+( startgroup "Validating outputs" ) 2> /dev/null
 
-    if [[ "${HOST_PLATFORM}" != "${BUILD_PLATFORM}" ]]; then
-        EXTRA_CB_OPTIONS="${EXTRA_CB_OPTIONS:-} --no-test"
-    fi
+validate_recipe_outputs "${FEEDSTOCK_NAME}"
 
-    conda mambabuild ./recipe -m ./.ci_support/${CONFIG}.yaml \
-        --suppress-variables ${EXTRA_CB_OPTIONS:-} \
-        --clobber-file ./.ci_support/clobber_${CONFIG}.yaml
-    ( startgroup "Validating outputs" ) 2> /dev/null
+( endgroup "Validating outputs" ) 2> /dev/null
 
-    validate_recipe_outputs "${FEEDSTOCK_NAME}"
-
-    ( endgroup "Validating outputs" ) 2> /dev/null
-
-    ( startgroup "Uploading packages" ) 2> /dev/null
-
-    if [[ "${UPLOAD_PACKAGES}" != "False" ]] && [[ "${IS_PR_BUILD}" == "False" ]]; then
-      upload_package --validate --feedstock-name="${FEEDSTOCK_NAME}" ./ ./recipe ./.ci_support/${CONFIG}.yaml
-    fi
-
-    ( endgroup "Uploading packages" ) 2> /dev/null
-fi
