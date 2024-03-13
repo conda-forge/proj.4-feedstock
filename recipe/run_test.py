3eb4a09dcb65c927097c1a6c9464ed3faf431694
Mike Taves <mwtoews@gmail.com>
2024-03-13 23:45:07 +1300

    Show more diagnostics in run_test.py

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -6,40 +6,66 @@ import shutil
 import subprocess
 import sys
 
-print("Running " + sys.argv[0] + " ...\n")
+print("PROJ test diagnostics ...\n")
 
 
-def run(cmd, input, print_debug=False):
+def print_env(name):
+    if name in os.environ:
+        print(f"{name} -> {os.environ[name]}")
+    else:
+        print(f"{name} is not set")
+
+
+os.environ["PROJ_DEBUG"] = "3"
+print_env("PROJ_DEBUG")
+print_env("PROJ_DATA")
+print_env("PROJ_NETWORK")
+print(f"which proj -> {shutil.which('proj')}")
+print(f"which cs2cs -> {shutil.which('cs2cs')}")
+if shutil.which("proj"):
+    print(subprocess.run("proj", text=True, capture_output=True).stderr.splitlines()[0])
+print()
+
+
+def run(cmd, input, print_inout=True, print_debug=False):
     args = cmd.split()
-    cmd0 = shutil.which(args[0])
-    if cmd0 is None:
-        sys.exit(f"first command not found: {args[0]}")
-    args[0] = cmd0
-    print(f"echo {input} | {' '.join(args)}")
-    env = os.environ.copy()
-    env["PROJ_DEBUG"] = "3"
-    p = subprocess.run(args, input=input, text=True, capture_output=True, env=env)
+    if print_inout:
+        print(f"echo {input} | {' '.join(args)}")
+    p = subprocess.run(args, input=input, text=True, capture_output=True)
     if print_debug:
         print("PROJ DEBUG output:")
         print(p.stderr)
-    print(p.stdout)
+    if print_inout:
+        print(p.stdout.strip())
     return p
+
+
+def run_twice(cmd, input):
+    r1 = run(cmd, input)
+    r2 = run(cmd, input, print_inout=False)
+    if r1.stdout != r2.stdout:
+        print("Different results! Here are the differences with PROJ_DEBUG=3:")
+        d = difflib.Differ()
+        e1 = r1.stderr.splitlines(keepends=True)
+        e2 = r2.stderr.splitlines(keepends=True)
+        sys.stdout.writelines(list(d.compare(e1, e2)))
+        print()
+    else:
+        print("(ran twice with the same output)")
 
 
 # should be the first CLI test to ensure cs2cs results don't change
 # See https://github.com/conda-forge/proj.4-feedstock/pull/139
-r1 = run("cs2cs EPSG:26915 +to EPSG:26715", "569704.57 4269024.67")
-r2 = run("cs2cs EPSG:26915 +to EPSG:26715", "569704.57 4269024.67")
-if r1.stdout != r2.stdout:
-    print("Different results! Here is the difference with PROJ_DEBUG=3:")
-    d = difflib.Differ()
-    e1 = r1.stderr.splitlines(keepends=True)
-    e2 = r2.stderr.splitlines(keepends=True)
-    sys.stdout.writelines(list(d.compare(e1, e2)))
-    print()
+run_twice("cs2cs EPSG:26915 +to EPSG:26715", "569704.57 4269024.67")
+print()
 
-_ = run("proj +proj=utm +zone=13 +ellps=WGS84", "-105 40")
-_ = run("cs2cs +proj=latlong +datum=NAD27 +to +proj=latlong +datum=NAD83", "-117 30")
-_ = run("cs2cs +init=epsg:4326 +to +init=epsg:2975", "-105 40")
+run("proj +proj=utm +zone=13 +ellps=WGS84", "-105 40")
+print()
 
-print("done")
+run("cs2cs +proj=latlong +datum=NAD27 +to +proj=latlong +datum=NAD83", "-117 30")
+print()
+
+run("cs2cs +init=epsg:4326 +to +init=epsg:2975", "-105 40")
+print()
+
+print(f"Done {sys.argv[0]}")
